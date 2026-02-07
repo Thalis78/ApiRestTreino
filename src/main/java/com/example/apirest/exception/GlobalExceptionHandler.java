9266b317d216dc2589345c9affa0be5784afb6ef
thalisson <thalissonmoura138@gmail.com>
2026-02-07 18:44:25 -0300

    :sparkles:feat: Adiciona exceção global para capturar mensagem do spring validation

--- src/main/java/com/example/apirest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/apirest/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.example.apirest.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestControllerAdvice
@@ -12,6 +15,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UsuarioNaoEncontradoException.class)
     public ResponseEntity<String> handleUsuarioNaoEncontrado(UsuarioNaoEncontradoException e){
         return ResponseEntity.status(404).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidation(MethodArgumentNotValidException e){
+        Map<String, String> erros = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error ->
+                erros.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest().body(erros);
     }
 
 }
